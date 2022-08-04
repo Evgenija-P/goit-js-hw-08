5cd2c1454fb065620d0cf864dd6072495513fbae
Evgenija-P <e.a.poduzova@gmail.com>
2022-08-04 22:59:44 +0300

    test 1 + 2, 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,35 +1,25 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('timeupdate', curentTime);
+player.on('timeupdate', throttle(onPlay, 1000));
 
-function curentTime() {}
+function onPlay(data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}
 
-// let curentTime = player
-//   .getCurrentTime()
-//   .then(function () {
-//     // seconds = the current playback position
-//   })
-//   .catch(function (error) {
-//     // an error occurred
-//   });
-// console.log();
+var curentTime = localStorage.getItem('videoplayer-current-time');
 
-// player
-//   .setCurrentTime(120.46)
-//   .then(function (seconds) {
-//     // seconds = the actual time that the player seeked to
-//   })
-//   .catch(function (error) {
-//     switch (error.name) {
-//       case 'RangeError':
-//         // the time was less than 0 or greater than the video’s duration
-//         break;
-
-//       default:
-//         // some other error occurred
-//         break;
-//     }
-//   });
+player
+  .setCurrentTime(curentTime)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+      default:
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,22 @@
+const form = document.querySelector(`.feedback-form`);
+const textArea = document.querySelector(`textarea`);
+// console.log(form, textArea);
 
+form.addEventListener(`input`, onForm);
+textArea.addEventListener(`blur`, onTextArea);
+
+function onTextArea(event) {
+  //   const textInput = event.currentTarget.value;
+  //   localStorage.setItem(`feedback-form-state`, textInput);
+}
+
+function onForm(event) {
+  const curentValue = {
+    email: event.currentTarget.value,
+    message: event.currentTarget.value,
+  };
+  console.log(curentValue);
+  //   localStorage.setItem(`feedback-form-state`, JSON.stringify(curentValue));
+  // const textInput = event.currentTarget.value;
+  // localStorage.setItem(`text-area`, textInput);
+}
