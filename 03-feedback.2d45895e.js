9cbeda182df06c26b50c620a6b2e27dd2312a5db
Evgenija-P <99011054+Evgenija-P@users.noreply.github.com>
2022-08-07 18:47:31 +0000

    Deploying to gh-pages from @ Evgenija-P/goit-js-hw-08@dd44dffccdf42209ae6b692ca44ce20579658fb3 🚀

--- 03-feedback.2d45895e.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".feedback-form"),t=document.querySelector("textarea");e.addEventListener("input",(function(e){var t={email:e.currentTarget.value,message:e.currentTarget.value};console.log(t)})),t.addEventListener("blur",(function(e){}))}();
-//# sourceMappingURL=03-feedback.2d45895e.js.map
--- 03-feedback.53cf975d.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".feedback-form"),t=document.querySelector("textarea");e.addEventListener("input",(function(e){const t={email:e.currentTarget.value,message:e.currentTarget.value};console.log(t)})),t.addEventListener("blur",(function(e){}));
-//# sourceMappingURL=03-feedback.53cf975d.js.map
